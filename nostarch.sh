6c8e34413ed340d346492ba04c44c8e0944926fe
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-08-26 16:43:48 -0700

    Add remove links script to the nostarch delivery script

--- nostarch.sh
+++ nostarch.sh
@@ -10,9 +10,10 @@ ls src/*.md | \
 grep -v SUMMARY.md | \
 # Extract just the filename so we can reuse it easily.
 xargs -n 1 basename | \
-# Change all the links from markdown to italicized inline text.
+# Remove all links followed by <!-- ignore -->, then
+# Change all remaining links from markdown to italicized inline text.
 while IFS= read -r filename; do
-  cargo run --bin link2print < "src/$filename" > "tmp/$filename"
+  < "src/$filename" cargo run --bin remove_links | cargo run --bin link2print > "tmp/$filename"
 done
 # Concat the files into the nostarch dir.
 cargo run --bin concat_chapters tmp nostarch
