6a898d9bd71dbde62cf344325ed24f11806dadf4
steveklabnik <steve@steveklabnik.com>
2017-03-03 12:08:16 -0500

    omg i am so bad at computers

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@ export PATH=$PATH:/home/travis/.cargo/bin;
 # tests for the second edition
 
 cd second-edition
-bash spellcheck.sh list || return false;
-mdbook test || return false;
-mdbook build || return false;
-cargo run --bin lfp src || return false;
+bash spellcheck.sh list || return 1;
+mdbook test || return 1;
+mdbook build || return 1;
+cargo run --bin lfp src || return 1;
