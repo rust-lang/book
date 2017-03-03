e319b90ca42c176d9cd93a6f530fd28a306ea452
steveklabnik <steve@steveklabnik.com>
2017-03-03 12:06:35 -0500

    fail the build if things go wrong

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@ export PATH=$PATH:/home/travis/.cargo/bin;
 # tests for the second edition
 
 cd second-edition
-bash spellcheck.sh list
-mdbook test
-mdbook build
-cargo run --bin lfp src
+bash spellcheck.sh list || return false;
+mdbook test || return false;
+mdbook build || return false;
+cargo run --bin lfp src || return false;
