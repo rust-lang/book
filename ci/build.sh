dfda7b849d86ff9308e501b042afdc5ce6b68381
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2017-03-17 10:02:35 -0400

    Fix the build to fail if spellchecking fails
    return: can only `return' from a function or sourced script

--- ci/build.sh
+++ ci/build.sh
@@ -8,12 +8,14 @@
 # option. This file may not be copied, modified, or distributed
 # except according to those terms.
 
+set -e
+
 export PATH=$PATH:/home/travis/.cargo/bin;
 
 # tests for the second edition
 
 cd second-edition
-bash spellcheck.sh list || return 1;
-mdbook test || return 1;
-mdbook build || return 1;
-cargo run --bin lfp src || return 1;
+bash spellcheck.sh list
+mdbook test
+mdbook build
+cargo run --bin lfp src
