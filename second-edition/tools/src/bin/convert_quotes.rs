41fd7822dc4cee3e7cc234d70f1e276e68fac850
Lance Lafontaine <lance.lafontaine92@gmail.com>
2017-04-27 23:46:58 -0400

    Simplifies convert_quotes script and reworks logic, should now be working as expected.

--- second-edition/tools/src/bin/convert_quotes.rs
+++ second-edition/tools/src/bin/convert_quotes.rs
@@ -2,7 +2,6 @@ use std::io;
 use std::io::{Read, Write};
 
 fn main () {
-    let mut is_in_code_block = false;
     let mut is_in_inline_code = false;
 
     let mut buffer = String::new();
@@ -13,59 +12,46 @@ fn main () {
     for line in buffer.lines() {
         // only checks for " in html tag on a single line, otherwise lots of formatting breaks.
         let mut is_in_html_tag = false;
+        let mut modified_line = &mut String::new();
+        let mut previous_char = std::char::REPLACEMENT_CHARACTER;
+        let mut chars_in_line = line.chars();
 
-        // check if inside code block
-        if line.starts_with("```") {
-            is_in_code_block = !is_in_code_block;
-        }
-        if is_in_code_block {
-            write!(io::stdout(), "{}\n", line).unwrap();
-        }
-        else {
-            let mut modified_line = &mut String::new();
-            let mut previous_char = std::char::REPLACEMENT_CHARACTER;
-            let mut chars_in_line = line.chars();
-
-            while let Some(possible_match) = chars_in_line.next() {
-                let char_to_push;
-                // check if inside inline code
-                if possible_match == '`' {
-                    is_in_inline_code = !is_in_inline_code;
-                }
-                // check if inside html tag
-                if possible_match == '<' {
-                    is_in_html_tag = true;
-                }
-                if possible_match == '>' {
-                    is_in_html_tag = false;
-                }
-
-                // replace with right/left apostrophe/quote
-                if possible_match == '\'' && !is_in_inline_code && !is_in_html_tag {
-                    if previous_char.is_alphanumeric() || previous_char == '‘' {
-                        char_to_push = '’';
-                    }
-                    else {
-                        char_to_push = '‘';
-                    }
-                }
-                else if possible_match == '"'  && !is_in_inline_code && !is_in_html_tag {
-                    if previous_char.is_alphanumeric() || previous_char == '“' {
-                        char_to_push = '”';
-                    }
-                    else {
-                        char_to_push = '“';
-                    }
-                }
-                // leave untouched
-                else {
-                    char_to_push = possible_match;
-                }
-                modified_line.push(char_to_push);
-                previous_char = char_to_push;
+        while let Some(possible_match) = chars_in_line.next() {
+            let char_to_push;
+            // check if inside inline code
+            if possible_match == '`' {
+                is_in_inline_code = !is_in_inline_code;
             }
-            write!(io::stdout(), "{}\n", modified_line).unwrap();
+            // check if inside html tag
+            if ((possible_match == '<') || (possible_match == '>')) && !is_in_inline_code {
+                is_in_html_tag = !is_in_html_tag;
+            }
+
+            // replace with right/left apostrophe/quote
+            if possible_match == '\'' && !is_in_inline_code && !is_in_html_tag {
+                if previous_char.is_alphanumeric() || previous_char == '‘' {
+                    char_to_push = '’';
+                }
+                else {
+                    char_to_push = '‘';
+                }
+            }
+            else if possible_match == '"'  && !is_in_inline_code && !is_in_html_tag {
+                if previous_char.is_alphanumeric() || previous_char == '“' {
+                    char_to_push = '”';
+                }
+                else {
+                    char_to_push = '“';
+                }
+            }
+            // leave untouched
+            else {
+                char_to_push = possible_match;
+            }
+            modified_line.push(char_to_push);
+            previous_char = char_to_push;
         }
+        write!(io::stdout(), "{}\n", modified_line).unwrap();
     }
 }
 
