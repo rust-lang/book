b1dae28b150508efede9a5bbb73ffe0f377c53cb
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2017-07-18 19:29:05 -0400

    Make a script to run convert quotes on multiple files

---
+++ second-edition/convert-quotes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright 2017 The Rust Project Developers. See the COPYRIGHT
+# file at the top-level directory of this distribution and at
+# http://rust-lang.org/COPYRIGHT.
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+set -eu
+
+mkdir -p tmp/nostarch
+
+for f in nostarch/*.md
+do
+    cat "$f" | cargo run --bin convert_quotes > "tmp/$f"
+    mv "tmp/$f" "$f"
+done
