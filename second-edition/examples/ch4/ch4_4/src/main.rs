e6c3041a61d413c7b4765813778383612af1cbc7
Anatolij <java1cprog@yandex.ru>
2018-08-25 11:00:25 +0200

    change code example
    Add println! marcros which help to understand what going on. Just "hello" string does not show anything. It would be better to have messages for demonstration ideas of described subject.

--- second-edition/examples/ch4/ch4_4/src/main.rs
+++ second-edition/examples/ch4/ch4_4/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let s1 = gives_ownership();         // gives_ownership moves its return
-                                        // value into s1.
+    println!("{}",s1);                  // value into s1.
 
-    let s2 = String::from("hello");     // s2 comes into scope.
-
+    let s2 = String::from("takes and gives back");     // s2 comes into scope.
+    println!("{}",s2);
     let s3 = takes_and_gives_back(s2);  // s2 is moved into
-                                        // takes_and_gives_back, which also
+    println!("{}",s3);                  // takes_and_gives_back, which also
                                         // moves its return value into s3.
 } // Here, s3 goes out of scope and is dropped. s2 goes out of scope but was
   // moved, so nothing happens. s1 goes out of scope and is dropped.
@@ -14,7 +14,7 @@ fn gives_ownership() -> String {             // gives_ownership will move its
                                              // return value into the function
                                              // that calls it.
 
-    let some_string = String::from("hello"); // some_string comes into scope.
+    let some_string = String::from("gives ownership"); // some_string comes into scope.
 
     some_string                              // some_string is returned and
                                              // moves out to the calling
