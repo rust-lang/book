01d8d86ab90ab7b18d305e8b0d92e520b45ed698
Artem Varaksa <aymfst@gmail.com>
2018-08-28 07:02:40 +0300

    Add Ferris Technology

---
+++ 2018-edition/ferris.js
@@ -0,0 +1,22 @@
+var ferrisTypes = [
+  'does_not_compile',
+  'panics',
+  'unsafe',
+]
+
+document.addEventListener('DOMContentLoaded', () => {
+  for (var ferrisType of ferrisTypes) {
+    makeFerris(ferrisType)
+  }
+})
+
+function makeFerris (type) {
+  var elements = document.getElementsByClassName(type)
+
+  for (var codeBlock of elements) {
+    var img = document.createElement("img")
+    img.setAttribute('src', 'img/ferris/' + type + '.svg')
+    img.className = 'ferris'
+    codeBlock.parentElement.insertBefore(img, codeBlock)
+  }
+}
