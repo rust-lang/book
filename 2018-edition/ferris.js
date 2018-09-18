691cb2fdc6c24b283dde7a71692d0a4a86a4412a
Artem Varaksa <aymfst@gmail.com>
2018-09-18 20:07:31 +0300

    Ferris no longer appears on code blocks with < 4 lines

--- 2018-edition/ferris.js
+++ 2018-edition/ferris.js
@@ -19,25 +19,33 @@ var ferrisTypes = [
 
 document.addEventListener('DOMContentLoaded', () => {
   for (var ferrisType of ferrisTypes) {
-    makeFerris(ferrisType)
+    attachFerrises(ferrisType)
   }
 })
 
-function makeFerris (type) {
+function attachFerrises (type) {
   var elements = document.getElementsByClassName(type.attr)
 
   for (var codeBlock of elements) {
-    var a = document.createElement('a')
-    a.setAttribute('href', 'ch00-00-introduction.html#ferris')
-    a.setAttribute('target', '_blank')
+    var lines = codeBlock.textContent.split(/\r|\r\n|\n/).length - 1;
 
-    var img = document.createElement('img')
-    img.setAttribute('src', 'img/ferris/' + type.attr + '.svg')
-    img.setAttribute('title', type.title)
-    img.className = 'ferris'
-
-    a.appendChild(img)
-
-    codeBlock.parentElement.insertBefore(a, codeBlock)
+    if (lines >= 4) {
+      attachFerris(codeBlock, type)
+    }
   }
+}
+
+function attachFerris (element, type) {
+  var a = document.createElement('a')
+  a.setAttribute('href', 'ch00-00-introduction.html#ferris')
+  a.setAttribute('target', '_blank')
+
+  var img = document.createElement('img')
+  img.setAttribute('src', 'img/ferris/' + type.attr + '.svg')
+  img.setAttribute('title', type.title)
+  img.className = 'ferris'
+
+  a.appendChild(img)
+
+  element.parentElement.insertBefore(a, element)
 }
