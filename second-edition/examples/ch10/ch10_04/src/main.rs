b6a3ee65a21e658d7861b11522986e1c2ad3f73d
Anatolij <java1cprog@yandex.ru>
2018-10-03 09:55:28 +0200

    add more examples

--- second-edition/examples/ch10/ch10_04/src/main.rs
+++ second-edition/examples/ch10/ch10_04/src/main.rs
@@ -5,13 +5,12 @@ struct Point<T> {
 }
 
 fn main() {
-/*
+
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
     println!("{:?}", integer);
     println!("{:?}", float);
-
-    */
+    
 
     let wont_work = Point { x: 5., y: 4.0 };
     println!("{:?}", wont_work);
