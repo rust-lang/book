6579c05cb9dad3020200c0a18d867417c8689fad
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-09-29 20:14:22 -0400

    Include code without hidden lines for nostarch files

--- tools/nostarch.sh
+++ tools/nostarch.sh
@@ -6,19 +6,22 @@ cargo build --release
 
 mkdir -p tmp
 rm -rf tmp/*.md
+rm -rf tmp/markdown
 
-# Get all the Markdown files in the src dir,
-ls src/${1:-""}*.md | \
-# except for `SUMMARY.md`.
-grep -v SUMMARY.md | \
+# Render the book as Markdown to include all the code listings
+MDBOOK_OUTPUT__MARKDOWN=1 mdbook build -d tmp
+
+# Get all the Markdown files
+ls tmp/markdown/${1:-""}*.md | \
 # Extract just the filename so we can reuse it easily.
 xargs -n 1 basename | \
 # Remove all links followed by `<!-- ignore -->``, then
 # Change all remaining links from Markdown to italicized inline text.
 while IFS= read -r filename; do
-  < "src/$filename" ./target/release/remove_links \
+  < "tmp/markdown/$filename" ./target/release/remove_links \
     | ./target/release/link2print \
-    | ./target/release/remove_markup > "tmp/$filename"
+    | ./target/release/remove_markup \
+    | ./target/release/remove_hidden_lines > "tmp/$filename"
 done
 # Concatenate the files into the `nostarch` dir.
 ./target/release/concat_chapters tmp nostarch
