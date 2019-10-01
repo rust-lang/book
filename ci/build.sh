5b2740e64a21183465f1632958e822f91859b145
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-09-30 20:29:07 -0400

    Remove the check for unstable features
    We don't use nightly Rust anymore, nor do we try to land docs for
    features while they're still unstable, so this check is no longer
    needed. The tests will just fail if we try to use a feature flag in code
    listings.

--- ci/build.sh
+++ ci/build.sh
@@ -4,13 +4,6 @@ set -e
 
 export PATH=$PATH:/home/travis/.cargo/bin;
 
-# Feature check
-cd ci/stable-check
-
-cargo run -- ../../src
-
-cd ../..
-
 echo 'Spellchecking...'
 bash ci/spellcheck.sh list
 echo 'Testing...'
