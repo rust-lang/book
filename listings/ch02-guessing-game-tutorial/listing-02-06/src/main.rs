eceb294549deb6247369b9d48551bda6991c8e43
Geert Stappers <geert.stappers@hendrikx-itc.nl>
2020-05-05 17:14:51 +0200

    listing 02 06  declare variable outside loop
    The final listing of the guessing game
    declares variable guess inside the guessing loop.
    
    It feels wrong to ask a compiler to declare the same variable
    each iteration of a loop. It feels more wrong to have that
    in a tutorial.
    
    Moved the declaration outside the loop.

--- listings/ch02-guessing-game-tutorial/listing-02-06/src/main.rs
+++ listings/ch02-guessing-game-tutorial/listing-02-06/src/main.rs
@@ -7,10 +7,10 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
+    let mut guess = String::new();
+
     loop {
         println!("Please input your guess.");
-
-        let mut guess = String::new();
 
         io::stdin()
             .read_line(&mut guess)
