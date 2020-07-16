3fbecc3dfad846433577d499565b6f9970ba9a7f
Niranjana Biju <42086667+niranjana687@users.noreply.github.com>
2020-07-16 13:48:12 +0530

    fn returns_summarizable()
    The listing in Returning Types that Implement Traits has a slight change.
    The code given is :
    fn returns_summarizable() -> impl Summary {
        Tweet {
            username: String::from("horse_ebooks"),
            content: String::from(
                "of course, as you probably already know, people",
            ),
            reply: false,
            retweet: false,
        }
    }
    
    The correction should be: 
    fn returns_summarizable() -> impl Summary {
        Tweet {
            username: String::from("horse_ebooks"),
            content: String::from(
                "of course, as you probably already know, people"),
            reply: false,
            retweet: false,
        }
    }

--- listings/ch10-generic-types-traits-and-lifetimes/no-listing-05-returning-impl-trait/src/lib.rs
+++ listings/ch10-generic-types-traits-and-lifetimes/no-listing-05-returning-impl-trait/src/lib.rs
@@ -33,8 +33,7 @@ fn returns_summarizable() -> impl Summary {
     Tweet {
         username: String::from("horse_ebooks"),
         content: String::from(
-            "of course, as you probably already know, people",
-        ),
+            "of course, as you probably already know, people"),
         reply: false,
         retweet: false,
     }
