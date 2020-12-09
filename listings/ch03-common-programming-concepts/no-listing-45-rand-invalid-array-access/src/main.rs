a9d71e43795174fe58b512faa5bd126e7a7cdeca
DoubleHyphen <36716576+DoubleHyphen@users.noreply.github.com>
2020-12-09 20:39:54 +0000

    Update main.rs

--- listings/ch03-common-programming-concepts/no-listing-45-rand-invalid-array-access/src/main.rs
+++ listings/ch03-common-programming-concepts/no-listing-45-rand-invalid-array-access/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     let a = [1, 2, 3, 4, 5];
-    let index = rand::random::<usize>();
+    let index = std::env::args()
+        .nth(1)
+        .and_then(|x| x.parse().ok())
+        .unwrap_or(15);
     let index = index | 15;
     let index = index & 15;
+// At this point, irrespective of the input,
+// index has the value 15.
 
     let element = a[index];
 
