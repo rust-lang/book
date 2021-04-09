65392aecb03187b63a6c1ae2465f92c2202b3991
Martin Logan <martinloganzz@gmail.com>
2021-04-09 10:20:38 -0700

    ch09-error-handling Fix file open example with match.

--- listings/ch09-error-handling/listing-09-04/src/main.rs
+++ listings/ch09-error-handling/listing-09-04/src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 fn main() {
     let f = File::open("hello.txt");
 
-    let f = match f {
+    match f {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file: {:?}", error),
     };
