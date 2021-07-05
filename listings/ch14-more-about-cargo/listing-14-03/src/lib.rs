rust-lang/book

commit eb60fedc9ccd72999f9aabd82f5936ca0143dd8f

A solution for empty main in lib.rs, I think!
The reasons we needed empty `fn main() {}`s were twofold:

- Avoid confusing people when they click the "expand" button on the code
listing and see the auto-main wrapping
- Avoid failing doctests when running `mdbook test` that don't work when
rustdoc wraps a code listing in main

I think I have a solution that mostly solves these cases.

I don't know why this didn't occur to me before. Here's my current
thinking in case these assumptions turn out to be wrong:

There are a [few things that tell mdbook to disable the
main-wrapping][mdbook], and I hadn't noticed one of them until now: if
you annotate a code block with `noplayground`, it won't add a `main`
around it (and it also won't have the "play" button in the upper right
that runs the block and inserts the result into the page).

So instead of putting an empty `fn main() {}` at the bottom of
src/lib.rs files that doesn't make sense, annotate those listings with
`noplayground`. I don't think anyone will miss the play button anyway
because:

- The play button doesn't run tests, so there wasn't any output for
these examples anyway
- If an example doesn't compile, we have it marked `ignore` so that it
doesn't make the tests fail, and `ignore` also disables the play button,
so there isn't a way to see compiler errors either

In most of these cases, `mdbook test` that runs these as doctests will
still wrap these in main, but the tests still pass.

There are some cases, mostly around modules and using `crate::` that
won't pass as doctests when wrapped in main. For those, I've annotated
them with the [undocumented][] [`test_harness`][] attribute that apparently
I was using at some point and then [stopped using][] and now I've
decided to use again, but maybe send in a PR to rust-lang/rust to
change the name to `no_main` and document it or something. In any case,
that shouldn't affect readers at all.

listings/ch14-more-about-cargo/listing-14-03/src/lib.rs
@@ -1,4 +1,3 @@
-// ANCHOR: here
 //! # Art
 //!
 //! A library for modeling artistic concepts.
@@ -31,6 +30,3 @@ pub mod utils {
         // ANCHOR: here
     }
 }
-// ANCHOR_END: here
-
-fn main() {}

listings/ch07-managing-growing-projects/listing-07-01/src/lib.rs
@@ -1,4 +1,3 @@
-// ANCHOR: here
 mod front_of_house {
     mod hosting {
         fn add_to_waitlist() {}
@@ -14,6 +13,3 @@ mod front_of_house {
         fn take_payment() {}
     }
 }
-// ANCHOR_END: here
-
-fn main() {}

listings/ch07-managing-growing-projects/listing-07-07/src/lib.rs
@@ -1,4 +1,3 @@
-// ANCHOR: here
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -12,6 +11,3 @@ pub fn eat_at_restaurant() {
     // Relative path
     front_of_house::hosting::add_to_waitlist();
 }
-// ANCHOR_END: here
-
-fn main() {}

listings/ch07-managing-growing-projects/listing-07-08/src/lib.rs
@@ -1,4 +1,3 @@
-// ANCHOR: here
 fn serve_order() {}
 
 mod back_of_house {
@@ -9,6 +8,3 @@ mod back_of_house {
 
     fn cook_order() {}
 }
-// ANCHOR_END: here
-
-fn main() {}

listings/ch07-managing-growing-projects/listing-07-11/src/lib.rs
@@ -1,4 +1,3 @@
-// ANCHOR: here
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -12,6 +11,3 @@ pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
 }
-// ANCHOR_END: here
-
-fn main() {}

listings/ch07-managing-growing-projects/listing-07-12/src/lib.rs
@@ -1,4 +1,3 @@
-// ANCHOR: here
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -12,6 +11,3 @@ pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
 }
-// ANCHOR_END: here
-
-fn main() {}

listings/ch07-managing-growing-projects/listing-07-13/src/lib.rs
@@ -1,4 +1,3 @@
-// ANCHOR: here
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -12,6 +11,3 @@ pub fn eat_at_restaurant() {
     add_to_waitlist();
     add_to_waitlist();
 }
-// ANCHOR_END: here
-
-fn main() {}

listings/ch07-managing-growing-projects/listing-07-17/src/lib.rs
@@ -1,4 +1,3 @@
-// ANCHOR: here
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -12,6 +11,3 @@ pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
 }
-// ANCHOR_END: here
-
-fn main() {}

commit 05a580c3266ac9134d3270e4312de12c92e6ae33

Fix code hiding that I broke in eb60fedc9
Pointed out in https://github.com/rust-lang/book/pull/2741

listings/ch14-more-about-cargo/listing-14-03/src/lib.rs
@@ -1,3 +1,4 @@
+// ANCHOR: here
 //! # Art
 //!
 //! A library for modeling artistic concepts.
@@ -26,7 +27,8 @@ pub mod utils {
     pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
         // --snip--
         // ANCHOR_END: here
-        SecondaryColor::Orange
+        unimplemented!();
         // ANCHOR: here
     }
 }
+// ANCHOR_END: here