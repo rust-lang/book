eceb40ba259e295841a81bc24dbaa008ab59209a
vwkd <33468089+vwkd@users.noreply.github.com>
2021-11-06 10:58:40 +0200

    Don‘t make side effect in match scrutinee

--- listings/ch09-error-handling/listing-09-05/src/main.rs
+++ listings/ch09-error-handling/listing-09-05/src/main.rs
@@ -7,9 +7,13 @@ fn main() {
     let f = match f {
         Ok(file) => file,
         Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
+            ErrorKind::NotFound => {
+                let f2 = File::create("hello.txt");
+
+                match f2 {
+                    Ok(fc) => fc,
+                    Err(e) => panic!("Problem creating the file: {:?}", e),
+                }
             },
             other_error => {
                 panic!("Problem opening the file: {:?}", other_error)
