89f8e7b6e74f3bfbec49be0ea29f25c3f7200707
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2022-02-09 21:21:09 -0500

    Change match guard example to actually be unexpressable with patterns alone
    Fixes #2868.

--- listings/ch18-patterns-and-matching/listing-18-26/src/main.rs
+++ listings/ch18-patterns-and-matching/listing-18-26/src/main.rs
@@ -3,8 +3,8 @@ fn main() {
     let num = Some(4);
 
     match num {
-        Some(x) if x < 5 => println!("less than five: {}", x),
-        Some(x) => println!("{}", x),
+        Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+        Some(x) => println!("The number {} is odd", x),
         None => (),
     }
     // ANCHOR_END: here
