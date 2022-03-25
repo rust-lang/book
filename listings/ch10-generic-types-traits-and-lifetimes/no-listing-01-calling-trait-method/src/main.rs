b3189092729687e7964da22d3f682a0b44de3bb0
bicarlsen <carlsen.bri@gmail.com>
2022-03-25 09:12:01 +0100

    Udated content to match actual tweet
    Updated content of horse_ebooks to match [actual tweet](https://twitter.com/horse_ebooks/status/228032106859749377?lang=en).

--- listings/ch10-generic-types-traits-and-lifetimes/no-listing-01-calling-trait-method/src/main.rs
+++ listings/ch10-generic-types-traits-and-lifetimes/no-listing-01-calling-trait-method/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
         content: String::from(
-            "of course, as you probably already know, people",
+            "unfortunately, as you probably already know, people",
         ),
         reply: false,
         retweet: false,
