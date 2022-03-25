13218847180808401b6417f992d4e2939eb87351
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2022-03-25 15:59:04 -0400

    Rename a separate example of serve_order to deliver_order
    To be less confusing and not appear to be related with earlier examples.
    
    Fixes #2659.

--- listings/ch07-managing-growing-projects/listing-07-08/src/lib.rs
+++ listings/ch07-managing-growing-projects/listing-07-08/src/lib.rs
@@ -1,9 +1,9 @@
-fn serve_order() {}
+fn deliver_order() {}
 
 mod back_of_house {
     fn fix_incorrect_order() {
         cook_order();
-        super::serve_order();
+        super::deliver_order();
     }
 
     fn cook_order() {}
