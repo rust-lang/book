cf8395a167b4689878fb8bdd480df0edc0b434a5
Aaron Reisman <lifeiscontent@users.noreply.github.com>
2022-04-15 18:15:27 -0700

    Update main.rs

--- listings/ch04-understanding-ownership/no-listing-08-reference-with-annotations/src/main.rs
+++ listings/ch04-understanding-ownership/no-listing-08-reference-with-annotations/src/main.rs
@@ -10,5 +10,5 @@ fn main() {
 fn calculate_length(s: &String) -> usize { // s is a reference to a String
     s.len()
 } // Here, s goes out of scope. But because it does not have ownership of what
-  // it refers to, nothing happens.
+  // it refers to, it is not dropped.
 // ANCHOR_END: here
