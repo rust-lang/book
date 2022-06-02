9aa691ae2c8e99eaa857e398e5de04939d7157c1
Bixin <97690649+BixinFromThisRealm@users.noreply.github.com>
2022-06-01 21:31:40 -0300

    Fix missing ampersand in listing 10-11

--- listings/ch10-generic-types-traits-and-lifetimes/listing-10-11/src/main.rs
+++ listings/ch10-generic-types-traits-and-lifetimes/listing-10-11/src/main.rs
@@ -4,7 +4,7 @@ struct Point<X1, Y1> {
 }
 
 impl<X1, Y1> Point<X1, Y1> {
-    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+    fn mixup<X2, Y2>(&self, other: Point<X2, Y2>) -> Point<X1, Y2> {
         Point {
             x: self.x,
             y: other.y,
