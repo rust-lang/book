a488a1b3cfdff5e612b83b7fed968572e3296c47
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2022-06-10 22:14:29 -0400

    Tweak rustfmt slightly for these listings

--- listings/ch13-functional-features/listing-13-07/src/main.rs
+++ listings/ch13-functional-features/listing-13-07/src/main.rs
@@ -6,18 +6,9 @@ struct Rectangle {
 
 fn main() {
     let mut list = [
-        Rectangle {
-            width: 10,
-            height: 1,
-        },
-        Rectangle {
-            width: 3,
-            height: 5,
-        },
-        Rectangle {
-            width: 7,
-            height: 12,
-        },
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
     ];
 
     list.sort_by_key(|r| r.width);
