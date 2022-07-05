2fe9eff266ecb46f108ccf6e1f63727bdad74fe0
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2022-07-04 21:10:12 -0400

    Update to Rust 1.62

--- listings/ch13-functional-features/listing-13-06/src/main.rs
+++ listings/ch13-functional-features/listing-13-06/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let list = vec![1, 2, 3];
     println!("Before defining closure: {:?}", list);
 
-    thread::spawn(move || {
-        println!("From thread: {:?}", list)
-    }).join().unwrap();
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
 }
