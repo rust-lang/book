178b4456dc6bf709153b060d7281537d81157b1b
Will Crichton <wcrichto@cs.stanford.edu>
2022-08-11 01:08:49 +0000

    Update telemetry server

--- telemetry/server/src/main.rs
+++ telemetry/server/src/main.rs
@@ -18,6 +18,12 @@ fn bug(data: Json<Value>, logs: &State<logs::LogFiles>) -> &'static str {
   "success"
 }
 
+#[rocket::post("/feedback", format = "json", data = "<data>")]
+fn feedback(data: Json<Value>, logs: &State<logs::LogFiles>) -> &'static str {
+  logs.append("feedback.log", &data.to_string()).unwrap();
+  "success"
+}
+
 #[rocket::get("/")]
 fn index() -> &'static str {
   "MIND OVER COMPUTER"
@@ -27,6 +33,6 @@ fn index() -> &'static str {
 fn rocket() -> _ {
   rocket::build()
     .attach(cors::CORS)
-    .manage(logs::LogFiles::new(vec!["answers.log", "bug.log"]))
-    .mount("/", rocket::routes![index, answers, bug, cors::all_options])
+    .manage(logs::LogFiles::new(vec!["answers.log", "bug.log", "feedback.log"]))
+    .mount("/", rocket::routes![index, answers, bug, feedback, cors::all_options])
 }
