ac36d6adbf3946cabc8c23029ab7b5fff6bcec95
Kaligule <Code@schauderbasis.de>
2022-08-24 22:53:08 +0200

    Fix wrong println!-syntax
    The old version throws an error: there is no argument named `contents`
    
    In other news: Oh boy, I really want to be able to say:
    "I made a contribution to the code in the rust book."

--- listings/ch12-an-io-project/listing-12-04/src/main.rs
+++ listings/ch12-an-io-project/listing-12-04/src/main.rs
@@ -17,6 +17,6 @@ fn main() {
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}");
+    println!("With text:\n{}", contents);
 }
 // ANCHOR_END: here
