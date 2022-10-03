63182ffb73058bc55c89c47b11d6b10f93cbbf2e
Marijn <40164828+MarijnRitzen@users.noreply.github.com>
2022-10-03 13:13:13 +0200

    Update lib.rs
    Since the value is between 0 and 1 it should be a fraction and not a percentage.

--- listings/ch15-smart-pointers/listing-15-20/src/lib.rs
+++ listings/ch15-smart-pointers/listing-15-20/src/lib.rs
@@ -23,14 +23,14 @@ where
     pub fn set_value(&mut self, value: usize) {
         self.value = value;
 
-        let percentage_of_max = self.value as f64 / self.max as f64;
+        let fraction_of_max = self.value as f64 / self.max as f64;
 
-        if percentage_of_max >= 1.0 {
+        if fraction_of_max >= 1.0 {
             self.messenger.send("Error: You are over your quota!");
-        } else if percentage_of_max >= 0.9 {
+        } else if fraction_of_max >= 0.9 {
             self.messenger
                 .send("Urgent warning: You've used up over 90% of your quota!");
-        } else if percentage_of_max >= 0.75 {
+        } else if fraction_of_max >= 0.75 {
             self.messenger
                 .send("Warning: You've used up over 75% of your quota!");
         }
