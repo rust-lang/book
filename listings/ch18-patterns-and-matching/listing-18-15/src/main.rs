8c32b050d71926c9001e05f1eaee99a51d16ed23
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2022-11-03 21:55:26 -0400

    Upgrade to Rust 1.63

--- listings/ch18-patterns-and-matching/listing-18-15/src/main.rs
+++ listings/ch18-patterns-and-matching/listing-18-15/src/main.rs
@@ -13,15 +13,13 @@ fn main() {
             println!("The Quit variant has no data to destructure.");
         }
         Message::Move { x, y } => {
-            println!(
-                "Move in the x direction {x} and in the y direction {y}"
-            );
+            println!("Move in the x direction {x} and in the y direction {y}");
         }
         Message::Write(text) => {
             println!("Text message: {text}");
         }
-        Message::ChangeColor(r, g, b) => println!(
-            "Change the color to red {r}, green {g}, and blue {b}",
-        ),
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {r}, green {g}, and blue {b}",)
+        }
     }
 }
