5c5979d6a2cc675e4f9663a6ae6bcd7d0b5e46d1
Daejung Kim <daejung.kim@lge.com>
2023-01-05 10:19:29 +0900

    suggestion to clarify the scope of references

--- listings/ch04-understanding-ownership/no-listing-13-reference-scope-ends/src/main.rs
+++ listings/ch04-understanding-ownership/no-listing-13-reference-scope-ends/src/main.rs
@@ -5,9 +5,8 @@ fn main() {
     let r1 = &s; // no problem
     let r2 = &s; // no problem
     println!("{} and {}", r1, r2);
-    // variables r1 and r2 will not be used after this point
 
-    let r3 = &mut s; // no problem
+    let r3 = &mut s; // no problem, variables r1 and r2 will not be used after this point
     println!("{}", r3);
     // ANCHOR_END: here
 }
