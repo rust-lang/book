6a23d9560da34112bddac8b34e80138a8a50a6e2
Poxxy <41162160+Poxxy@users.noreply.github.com>
2023-01-27 10:20:28 -0500

    Include rectangle rotation for fn can_hold
    In the example used for function can_hold the test to check is to see if the first rectangle has greater width than the second rectangle and greater height than the second rectangle. My proposal is to also allow the case that the second rectangle fits by "rotating" the rectangle so to speak. An example is if rectangle a has dimensions 10x5 and rectangle b has dimensions 4x6, then rectangle b would fit into rectangle a with this proposed change, but would not with the current function.

--- listings/ch05-using-structs-to-structure-related-data/listing-05-15/src/main.rs
+++ listings/ch05-using-structs-to-structure-related-data/listing-05-15/src/main.rs
@@ -12,6 +12,7 @@ impl Rectangle {
 
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+        || self.width > other.height && self.height > other.width
     }
 }
 // ANCHOR_END: here
