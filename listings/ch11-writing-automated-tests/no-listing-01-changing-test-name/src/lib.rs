bb7aa19d9dc6fd9c9a2128e9526ae861602651d9
Jon Earnshaw <jon@smkdigital.co.uk>
2023-02-07 04:49:10 +0000

    Update listings subsequent to listing 11-1 to reflect current content

--- listings/ch11-writing-automated-tests/no-listing-01-changing-test-name/src/lib.rs
+++ listings/ch11-writing-automated-tests/no-listing-01-changing-test-name/src/lib.rs
@@ -1,7 +1,14 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn exploration() {
-        assert_eq!(2 + 2, 4);
+        let result = add(2, 2);
+        assert_eq!(result, 4);
     }
 }
