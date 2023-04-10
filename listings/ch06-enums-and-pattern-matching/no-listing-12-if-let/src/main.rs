b711ac0ad7e011f3079db9defd197d8a0fceb985
Alexander van Trijffel <alexander@structura.ws>
2023-04-10 21:59:04 +0200

    Update main.rs

--- listings/ch06-enums-and-pattern-matching/no-listing-12-if-let/src/main.rs
+++ listings/ch06-enums-and-pattern-matching/no-listing-12-if-let/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     // ANCHOR: here
-    let config_max = Some(3u8);
+    let config_max: Option<u8> = Some(3u8);
     if let Some(max) = config_max {
         println!("The maximum is configured to be {}", max);
     }
