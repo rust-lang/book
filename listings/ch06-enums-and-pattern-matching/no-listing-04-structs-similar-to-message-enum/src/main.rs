77b66af149d04b80f4d99ab735a47dc73b193439
Jun-Hee-Lee <kkami920523@gmail.com>
2023-06-20 17:41:24 +0900

    ch06-01: remove typo in no-listing-04

--- listings/ch06-enums-and-pattern-matching/no-listing-04-structs-similar-to-message-enum/src/main.rs
+++ listings/ch06-enums-and-pattern-matching/no-listing-04-structs-similar-to-message-enum/src/main.rs
@@ -4,7 +4,7 @@ struct MoveMessage {
     x: i32,
     y: i32,
 }
-struct WriteMessage(String); // tuple struct
+struct WriteMessage(String);
 struct ChangeColorMessage(i32, i32, i32); // tuple struct
                                           // ANCHOR_END: here
 
