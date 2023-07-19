a80a425f71f0849af91be3d7d19df7f6ed99cc4b
Mike Ton <mike.ton@gmail.com>
2023-07-19 16:46:31 +0000

    Comments cleaned up and removed

--- listings/ch16-fearless-concurrency/listing-16-15/src/main.rs
+++ listings/ch16-fearless-concurrency/listing-16-15/src/main.rs
@@ -5,12 +5,6 @@ fn main() {
     let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
-    // The chapters accompanying text states that the print should be :
-    // Result: 10
-    // But the current code yields :
-    // Result: 9
-    // An update to this should resolve the drift between text and code :
-    // 1..=10 - is a range that is inclusive of 1 and inclusive of 10
     for _ in 0..=10 {
         let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
