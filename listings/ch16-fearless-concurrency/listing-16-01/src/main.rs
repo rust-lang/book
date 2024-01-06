034262c3fefa75dca3603e6dad8ca6ccee32b239
Dmitrii Ogn <dimitriy_rudenko@mail.ru>
2024-01-07 01:41:17 +0300

    Update main.rs
    As far, as book refers to child thread to count from one to five, it should have enumeration from 1 to 5

--- listings/ch16-fearless-concurrency/listing-16-01/src/main.rs
+++ listings/ch16-fearless-concurrency/listing-16-01/src/main.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 
 fn main() {
     thread::spawn(|| {
-        for i in 1..10 {
+        for i in 1..5 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
         }
