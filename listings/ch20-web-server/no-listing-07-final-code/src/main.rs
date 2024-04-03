a14b54050c7cb425ed10037d237b49e73818914c
Chris Jansen <chris+git@claypotfrog.com>
2024-04-03 17:12:03 -0400

    Updated listings based on `listing-20-24`
    Updated `main.rs` in both `listing-20-25` and `no-listing-07-final-code` based on `listing-20-24`

--- listings/ch20-web-server/no-listing-07-final-code/src/main.rs
+++ listings/ch20-web-server/no-listing-07-final-code/src/main.rs
@@ -38,13 +38,8 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        length,
-        contents
-    );
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
 }
