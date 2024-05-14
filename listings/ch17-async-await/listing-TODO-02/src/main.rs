8e7420e2a18747d8760398a3caea161ce6bc3cee
Chris Krycho <hello@chriskrycho.com>
2024-05-14 15:59:10 -0600

    Ch. 17: start in earnest on §2, showing relation to threads
    Add some listings which I can actually run, with `TODO` paths in them
    since I do not know what the numbers will be, since I have not actually
    finished with §0 or §1 yet!

---
+++ listings/ch17-async-await/listing-TODO-02/src/main.rs
@@ -0,0 +1,19 @@
+use std::time::Duration;
+
+fn main() {
+    trpl::block_on(async {
+        let handle = trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(1)).await;
+            }
+        });
+
+        for i in 1..5 {
+            println!("hi number {i} from the second task!");
+            trpl::sleep(Duration::from_millis(1)).await;
+        }
+
+        handle.await;
+    });
+}
---
+++ listings/ch17-async-await/listing-TODO-03/src/main.rs
@@ -0,0 +1,21 @@
+use std::time::Duration;
+
+fn main() {
+    trpl::block_on(async {
+        let fut1 = async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(1)).await;
+            }
+        };
+
+        let fut2 = async {
+            for i in 1..5 {
+                println!("hi number {i} from the second task!");
+                trpl::sleep(Duration::from_millis(1)).await;
+            }
+        };
+
+        trpl::join(fut1, fut2).await;
+    });
+}
