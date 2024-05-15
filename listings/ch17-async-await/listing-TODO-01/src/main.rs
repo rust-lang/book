1ee7975b247d0c00dace93257f7cd15355687d48
Chris Krycho <hello@chriskrycho.com>
2024-05-15 09:31:53 -0600

    Ch. 17: add more examples and explanation to §02.

--- listings/ch17-async-await/listing-TODO-01/src/main.rs
+++ listings/ch17-async-await/listing-TODO-01/src/main.rs
@@ -1,7 +1,11 @@
+// ANCHOR: all
 use std::time::Duration;
 
+// ANCHOR: block_on
 fn main() {
     trpl::block_on(async {
+        // ANCHOR_END: block_on
+        // ANCHOR: task
         trpl::spawn_task(async {
             for i in 1..10 {
                 println!("hi number {i} from the first task!");
@@ -13,5 +17,9 @@ fn main() {
             println!("hi number {i} from the second task!");
             trpl::sleep(Duration::from_millis(1)).await;
         }
+        // ANCHOR_END: task
+        // ANCHOR: block_on
     });
 }
+// ANCHOR_END: block_on
+// ANCHOR_END: all
