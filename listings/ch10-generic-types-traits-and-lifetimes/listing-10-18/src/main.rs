e25a9a4e8d914a5facd002a77067218c9700dad4
Zihuan Teng <55567729+FrancisDrakeII@users.noreply.github.com>
2024-08-12 12:23:52 -0500

    Update main.rs
    The lifetime of x and r is wrong, here's the update

--- listings/ch10-generic-types-traits-and-lifetimes/listing-10-18/src/main.rs
+++ listings/ch10-generic-types-traits-and-lifetimes/listing-10-18/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let x = 5;            // ----------+-- 'b
+    let x = 5;            // ----------+-- 'b ('b is the lifetime of `x`)
                           //           |
-    let r = &x;           // --+-- 'a  |
+    let r = &x;           // --+-- 'a  |      ('a is the lifetime of `r`)
                           //   |       |
-    println!("r: {r}");   //   |       |
-                          // --+       |
-}                         // ----------+
+    println!("r: {r}");   //   |       |      (use the lifetime of `r`)
+                          //   |       |
+}                         // --+-------+      (end of both `r` and `x`)
