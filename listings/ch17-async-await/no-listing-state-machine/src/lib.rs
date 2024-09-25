0388426c51de9be7f489ccff9701f0d1e3d4747f
Chris Krycho <hello@chriskrycho.com>
2024-09-25 15:19:16 -0600

    Ch. 17: Fix no-listing code for state machine enum

--- listings/ch17-async-await/no-listing-state-machine/src/lib.rs
+++ listings/ch17-async-await/no-listing-state-machine/src/lib.rs
@@ -2,6 +2,7 @@ extern crate trpl; // required for mdbook test
 
 // ANCHOR: enum
 enum PageTitleFuture<'a> {
+    Initial { url: &'a str },
     GetAwaitPoint { url: &'a str },
     TextAwaitPoint { response: trpl::Response },
 }
