34820ac2f46fa2ffa2ee7d44ecaa3c33e227585e
Chris Krycho <hello@chriskrycho.com>
2025-02-25 10:32:11 -0700

    Edition maintenance: scripting the updates for future work

--- tools/update-editions.sh
+++ tools/update-editions.sh
@@ -2,7 +2,18 @@
 
 set -eu
 
+# So that we can do this in a single pass, check *here* if there are any “dirty”
+# files from Git, and bail if so.
+git diff --quiet || { echo "Git working directory is not clean"; exit 1; }
+
 OLD_EDITION=2021
 NEW_EDITION=2024
 
-find listings/** -name "Cargo.toml" -exec sed -i '' "s/edition = \"$OLD_EDITION\"/edition = \"$NEW_EDITION\"/g" '{}' \;
+# Start by preparing all listings for the edition.
+find listings -name "Cargo.toml" -exec cargo fix --allow-dirty --edition --manifest-path '{}' \;
+
+# Update the edition itself
+find listings -name "Cargo.toml" -exec sed -i '' "s/edition = \"$OLD_EDITION\"/edition = \"$NEW_EDITION\"/g" '{}' \;
+
+# Update all id
+find listings -name "Cargo.toml" -exec cargo fix --allow-dirty --edition-idioms --manifest-path {} \;
