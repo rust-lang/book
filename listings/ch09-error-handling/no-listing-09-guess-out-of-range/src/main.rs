8c8ef1e1a2bfc21696dacde0c444c18c484b3298
chuksgpfr <chuksgpfr@gmail.com>
2025-03-02 21:07:31 +0100

    changed rand deprecated function

--- listings/ch09-error-handling/no-listing-09-guess-out-of-range/src/main.rs
+++ listings/ch09-error-handling/no-listing-09-guess-out-of-range/src/main.rs
@@ -5,7 +5,7 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::rng().random_range(1..=100);
 
     // ANCHOR: here
     loop {
