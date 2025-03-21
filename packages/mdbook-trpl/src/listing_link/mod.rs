d41aec98043b0ff5e00eb8a942cf5e8aa1859824
mianlang <mianlang@foxmail.com>
2025-03-22 00:29:17 +0800

    handle cross-chapter reference

--- packages/mdbook-trpl/src/listing_link/mod.rs
+++ packages/mdbook-trpl/src/listing_link/mod.rs
@@ -5,6 +5,7 @@ use mdbook::{
     BookItem,
 };
 use regex::Regex;
+use std::collections::HashMap;
 
 pub struct ListingLinkPreprocessor;
 
@@ -14,11 +15,34 @@ impl Preprocessor for ListingLinkPreprocessor {
     }
 
     fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book> {
-        let re = Regex::new(r"Listing\s(\d+-\d+)").unwrap();
+        let number_re = Regex::new(r#"id="listing-(\d+-\d+)"#).unwrap();
+        let listing_re = Regex::new(r"Listing\s(\d+-\d+)").unwrap();
+        let mut listings: HashMap<String, String> = HashMap::new();
 
+        // Collect all number patterns and their corresponding filenames
         book.for_each_mut(|item| {
             if let BookItem::Chapter(ref mut chapter) = item {
-                chapter.content = re.replace_all(&chapter.content, r##"<a href="#listing-$1" class="listing-link">Listing $1</a>"##).to_string();
+                if let Some(ref path) = chapter.path {
+                    let filename = path.to_string_lossy().to_string();
+                    for cap in number_re.captures_iter(&chapter.content) {
+                        let number = cap[1].to_string();
+                        listings.insert(number, filename.clone());
+                    }
+                }
+            }
+        });
+
+        // Replace listing references with absolute href with filename and listing number hash
+        book.for_each_mut(|item| {
+            if let BookItem::Chapter(ref mut chapter) = item {
+                chapter.content = listing_re.replace_all(&chapter.content, |caps: &regex::Captures| {
+                    let listing_number = &caps[1];
+                    let href = listings.get(listing_number).map_or_else(
+                        || format!("#listing-{}", listing_number),
+                        |file| format!("{}#listing-{}", file, listing_number)
+                    );
+                    format!(r##"<a href="{}" class="listing-link">Listing {}</a>"##, href, listing_number)
+                }).to_string();
             }
         });
 
