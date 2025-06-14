edf61217563e1753383b62504e33ffc3dd5cddf4
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2025-06-14 11:34:11 -0400

    Adjust listings to reflect new main/lib split

--- listings/ch12-an-io-project/listing-12-15/src/main.rs
+++ listings/ch12-an-io-project/listing-12-15/src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
+use std::error::Error;
+use std::fs;
 use std::process;
 
-use minigrep::Config;
+use minigrep::search;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,8 +13,36 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
 }
--- listings/ch12-an-io-project/listing-12-24/src/main.rs
+++ listings/ch12-an-io-project/listing-12-24/src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
+use std::error::Error;
+use std::fs;
 use std::process;
 
-use minigrep::Config;
+use minigrep::{search, search_case_insensitive};
 
 // ANCHOR: here
 fn main() {
@@ -12,9 +14,50 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
 // ANCHOR_END: here
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
