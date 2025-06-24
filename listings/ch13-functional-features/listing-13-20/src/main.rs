e2ac919f8f0fd0c21a8cdb5022fe9582231229cd
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2025-06-24 14:10:53 -0400

    Address ch 13 tech review comments

--- listings/ch13-functional-features/listing-13-20/src/main.rs
+++ listings/ch13-functional-features/listing-13-20/src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
+use std::error::Error;
+use std::fs;
 use std::process;
 
-use minigrep::Config;
+use minigrep::{search, search_case_insensitive};
 
 fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err| {
@@ -9,8 +11,58 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+// ANCHOR: here
+impl Config {
+    fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
+}
+// ANCHOR_END: here
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
 }
