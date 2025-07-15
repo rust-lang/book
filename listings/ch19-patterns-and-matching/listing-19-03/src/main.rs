e694be5991f1c5cf65c654ed610b4049e2389ed1
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2025-07-15 18:56:50 -0400

    Backport changes to md

--- listings/ch19-patterns-and-matching/listing-19-03/src/main.rs
+++ listings/ch19-patterns-and-matching/listing-19-03/src/main.rs
@@ -1,9 +1,19 @@
 fn main() {
-    // ANCHOR: here
-    let v = vec!['a', 'b', 'c'];
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
 
-    for (index, value) in v.iter().enumerate() {
-        println!("{value} is at index {index}");
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
     }
-    // ANCHOR_END: here
 }
