d18973b6300031a72f96962e44ee5d4df33e34c8
Mohit Nandaniya <mohitnandaniya@Mohits-MacBook-Air.local>
2025-11-25 23:24:15 +0530

    Update: new version rand -> 0.9.2 and change code syntax.

--- listings/ch02-guessing-game-tutorial/listing-02-04/src/main.rs
+++ listings/ch02-guessing-game-tutorial/listing-02-04/src/main.rs
@@ -2,14 +2,14 @@
 use std::cmp::Ordering;
 use std::io;
 
-use rand::Rng;
+use rand::random_range;
 
 fn main() {
     // --snip--
     // ANCHOR_END: here
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = random_range(1..=100);
 
     println!("The secret number is: {secret_number}");
 
