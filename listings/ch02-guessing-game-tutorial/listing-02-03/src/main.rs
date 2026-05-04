c14c2254f55896e85acbdd85fd99b028bcfc2a15
M. Furkan Keskin <mfurkan.keskin1@gmail.com>
2026-05-05 02:05:14 +0300

    Update rand example to fix breaking changes in v0.10
    update rand version

--- listings/ch02-guessing-game-tutorial/listing-02-03/src/main.rs
+++ listings/ch02-guessing-game-tutorial/listing-02-03/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     println!("Guess the number!");
 
     // ANCHOR: ch07-04
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = (rng.next_u32() % 100) as u32 + 1;
     // ANCHOR_END: ch07-04
 
     println!("The secret number is: {secret_number}");
